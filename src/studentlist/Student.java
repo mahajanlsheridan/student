91583c5e7a7ba22948e8a7010a159d6775f3dd37
ronak111 <ronak.sheth@sheridancollege.ca>
2022-01-26 08:50:41 -0500

    Added new variable adress

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -5,11 +5,21 @@ public class Student {
     
     private String studentId;
     private String studentName;
+    private String adress;
     
     public Student(String studentId,String studentName){
         this.studentId = studentId;
         this.studentName=studentName;                
     }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+    
     public String getStudentId() {
         return studentId;
     }
